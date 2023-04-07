f3f88e9c0e879704bbf2ab9a87b8a44f47165cbb
Kyle Surowiec <kylesurowiec@me.com>
2023-04-07 16:10:25 -0500

    Cursor update

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.cmd(":set termguicolors")
+vim.cmd(":hi Cursor guifg=orange guibg=orange")
+vim.cmd(":hi Cursor2 guifg=teal guibg=teal")
+vim.cmd(
+  "set guicursor=n-v-c:block-Cursor/lCursor,i-ci-ve:block-Cursor2/lCursor2,r-cr:hor20,o:hor50"
+)
