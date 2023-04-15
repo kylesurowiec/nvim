b535790b5e36d58304638764e90c3500c212eeb6
Kyle Surowiec <kylesurowiec@me.com>
2023-04-15 03:00:21 -0500

    ...

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,3 +1,6 @@
 return {
   { "rcarriga/nvim-notify", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "L3MON4D3/LuaSnip", enabled = false },
+  { "folke/noice.nvim", enabled = false },
 }
