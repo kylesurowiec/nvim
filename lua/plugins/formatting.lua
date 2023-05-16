a378c2d36666fd67da62cc872de4abc26ae7a284
Kyle Surowiec <kylesurowiec@me.com>
2023-05-16 16:07:56 -0500

    fix prettier/eslint

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -3,7 +3,19 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        eslint = {},
         tailwindcss = {},
+      },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingPrettier = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingPrettier = false
+            end
+          end)
+        end,
       },
     },
   },
