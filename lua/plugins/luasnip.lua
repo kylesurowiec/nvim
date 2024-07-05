16d072329c4b4463248b1e258e01a0ae5afc0219
Kyle Surowiec <kylesurowiec@me.com>
2024-07-05 01:24:23 -0600

    ...

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,13 +1,6 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-<<<<<<< HEAD
-    enabled = true,
-    opts = {
-      history = false,
-    },
-=======
     enabled = false,
->>>>>>> e82f656 (fix config)
   },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -5,10 +5,6 @@ return {
       ensure_installed = {
         "omnisharp",
         "rust-analyzer",
-<<<<<<< HEAD
-        "tailwindcss-language-server",
-=======
->>>>>>> e82f656 (fix config)
       },
     },
   },
