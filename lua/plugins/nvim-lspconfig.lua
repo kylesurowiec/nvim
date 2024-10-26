85aa188d3d96996b267302c543555c3477626b90
Kyle Surowiec <kylesurowiec@me.com>
2024-10-26 14:55:32 -0600

    disable inlay hints

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -7,17 +7,11 @@ return {
       { "Hoffs/omnisharp-extended-lsp.nvim" },
     },
     opts = function(_, opts)
-      opts.servers = opts.servers or {}
-
-      opts.servers.rust_analyzer = {
-        settings = {
-          ["rust-analyzer"] = {
-            inlayHnts = {
-              enable = false,
-            },
-          },
-        },
+      opts.inlay_hints = {
+        enabled = false,
       }
+
+      opts.servers = opts.servers or {}
 
       opts.servers.vtsls = {
         settings = {
